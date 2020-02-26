0df25a1f8720b396904db87b4bbed3e97dfbd2e1
Emma <emmalhb@btinternet.com>
2020-02-26 17:37:27 +0000

    message

---
+++ 2d lists.py
@@ -0,0 +1,11 @@
+my2dlist=[[2,5,8],[3,7,4],[1.6,9],[4,2,0]]
+userrow=int(input('what row do you want displayed'))
+print(*my2dlist[userrow])
+usercolumn=int(input('which column in this row do you want displayed'))
+print(my2dlist[userrow][usercolumn])
+userchoice=input('do you want to chage that value').upper
+if userchoice=="Yes":
+    replacement=int(input('what do you want to replace it with'))
+    my2dlist[userrow][usercolumn]=replacement
+    print(my2dlist[userrow][usercolumn]) 
+    
