04149dbdb9c85a5705416144681d3a4196ee0070
patch153 <emmalhb@btinternet.com>
2020-05-06 17:38:31 +0100

    testing new computer git setup

--- 2d lists.py
+++ 2d lists.py
@@ -4,6 +4,8 @@ print(*my2dlist[userrow])
 usercolumn=int(input('which column in this row do you want displayed'))
 print(my2dlist[userrow][usercolumn])
 userchoice=input('do you want to chage that value').upper
+
+# Github is working with my new laptop
 if userchoice=="Yes":
     replacement=int(input('what do you want to replace it with'))
     my2dlist[userrow][usercolumn]=replacement
